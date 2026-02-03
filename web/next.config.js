62e66fd0ea70d609549daccc8d820b4e6f25091c
pranay5255 <pranay5255@yahoo.com>
2026-02-03 23:15:15 +0530

    Update web/next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  // Match the Vercel Output Directory setting and avoid static export mode.
+  distDir: 'dist',
+  output: 'standalone'
 };
 
 module.exports = nextConfig;
